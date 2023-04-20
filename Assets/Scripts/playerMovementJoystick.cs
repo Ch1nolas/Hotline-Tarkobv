95371c5bae9c1e90cc16748dccd8476993acdde7
Vitale Santino <s.vitale@etec.local>
2023-04-20 12:10:29 -0300

    feat: pj1 animations

--- Assets/Scripts/playerMovementJoystick.cs
+++ Assets/Scripts/playerMovementJoystick.cs
@@ -5,20 +5,49 @@ using UnityEngine;
 public class playerMovementJoystick : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    private int xActual;
+    private int yActual;
+    public Animator anim;
     public MovementJoystick movementJoystick;
     public Rigidbody2D rb;
     float moveSpeed = 250f;
+
+    float timer;
+    Vector2 vectorNew ;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        xActual = (int) transform.position.x;
+        yActual = (int) transform.position.y;
     }
 
-
+    void Update(){
+            // if(xActual == transform.position.x){
+            //     anim.SetTrigger("Idle");
+            // }
+            if(xActual < (int)transform.position.x)
+            {
+                anim.SetTrigger("Derecha");
+            } 
+            
+            if(xActual > (int)transform.position.x){
+                anim.SetTrigger("Izquierda");
+            } 
+            if(yActual < (int)transform.position.y)
+            {
+                anim.SetTrigger("Arriba");
+            } 
+            
+            if(yActual > (int)transform.position.y){
+                anim.SetTrigger("Abajo");
+            } 
+            xActual = (int) transform.position.x;
+            yActual = (int) transform.position.y;
+            Debug.Log(xActual);
+    }
     void FixedUpdate()
     {
-        //Debug.Log($"{movementJoystick.joystickVec.x},{movementJoystick.joystickVec.y}");
-        Vector2 vectorNew = new Vector2(movementJoystick.joystickVec.x * moveSpeed, movementJoystick.joystickVec.y * moveSpeed); 
+        vectorNew = new Vector2(movementJoystick.joystickVec.x * moveSpeed, movementJoystick.joystickVec.y * moveSpeed); 
         rb.AddForce(vectorNew); 
     }
 }
