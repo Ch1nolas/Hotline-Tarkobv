a75671839d63f78e158516c508a0c0f1eb06fc5f
Ch1nolas <nmartinez252524342@gmail.com>
2023-05-07 19:50:38 -0300

    A previous version of the character movement code has been uploaded.

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
-    public Transform target;
-    public float speed;
+    public Transform target;  // Referencia al transform del jugador
+    public float movementSpeed = 5f;  // Velocidad de movimiento del enemigo
 
 
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        // Verificar si el jugador está disponible
+        if (target != null)
+        {
+            // Calcular la dirección hacia el jugador
+            Vector3 direction = target.position - transform.position;
+            direction.Normalize();
+
+            // Mover el enemigo hacia el jugador
+            transform.position += direction * movementSpeed * Time.deltaTime;
+        }
     }
 }
